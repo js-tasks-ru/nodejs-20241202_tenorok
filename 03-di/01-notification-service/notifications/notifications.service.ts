1c5daba1041ceb08c3c9f34455f2bcafc97d26f1
tenorok <mail@tenorok.ru>
2024-12-19 21:16:01 +0300

    ДЗ 3.1: Реализация сервиса уведомлений

--- 03-di/01-notification-service/notifications/notifications.service.ts
+++ 03-di/01-notification-service/notifications/notifications.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, BadRequestException } from "@nestjs/common";
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+  sendEmail(to: string, subject: string, message: string): void {
+    if (!subject) {
+      throw new BadRequestException("Отсутствует тема для email");
+    }
+
+    if (!message) {
+      throw new BadRequestException("Отсутствует текст для email");
+    }
+
+    console.log(`Email sent to ${to}: [${subject}] ${message}`);
+  }
+
+  sendSMS(to: string, message: string): void {
+    if (!message) {
+      throw new BadRequestException("Отсутствует текст для SMS");
+    }
+
+    console.log(`SMS sent to ${to}: ${message}`);
+  }
+}
