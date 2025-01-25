6c8e020e928df9a98be02d0087fe5ac1f3240769
tenorok <mail@tenorok.ru>
2025-01-25 15:29:31 +0300

    ДЗ 5.1: Реализация CRUD для задач

--- 05-db/01-crud-tasks/tasks/entities/task.entity.ts
+++ 05-db/01-crud-tasks/tasks/entities/task.entity.ts
@@ -1,1 +1,23 @@
-export class Task {}
+import { IsString, IsBoolean, IsDefined, IsOptional } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsDefined()
+  @IsString()
+  title: string;
+
+  @Column()
+  @IsDefined()
+  @IsString()
+  description: string;
+
+  @Column({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  isCompleted?: boolean;
+}
--- 05-db/01-crud-tasks/tasks/tasks.controller.ts
+++ 05-db/01-crud-tasks/tasks/tasks.controller.ts
@@ -1,22 +1,84 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  UseFilters,
+  Param,
+  ParseIntPipe,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
+import { TypeOrmFilter } from "../filters/typeorm-error.filter";
 import { TasksService } from "./tasks.service";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 
+@UseFilters(new TypeOrmFilter())
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create() {}
+  async create(@Body() task: CreateTaskDto) {
+    const createdTask = await this.tasksService.create(task);
+
+    if (createdTask instanceof Error) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          error: createdTask.name,
+          message: createdTask.message,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return createdTask;
+  }
 
   @Get()
-  findAll() {}
+  findAll() {
+    return this.tasksService.findAll();
+  }
 
   @Get(":id")
-  findOne() {}
+  async findOne(@Param("id", ParseIntPipe) id: number) {
+    const task = await this.tasksService.findOne(id);
+
+    if (!task) {
+      throw new HttpException("Task not found", HttpStatus.NOT_FOUND);
+    }
+
+    return task;
+  }
 
   @Patch(":id")
-  update() {}
+  async update(
+    @Param("id", ParseIntPipe) id: number,
+    @Body() task: UpdateTaskDto,
+  ) {
+    const updatedTask = await this.tasksService.update(id, task);
+
+    if (!updatedTask) {
+      throw new HttpException("Task not found", HttpStatus.NOT_FOUND);
+    }
+
+    return updatedTask;
+  }
 
   @Delete(":id")
-  remove() {}
+  async remove(@Param("id", ParseIntPipe) id: number) {
+    const result = await this.tasksService.remove(id);
+
+    if (result.affected === 0) {
+      throw new HttpException("Task not found", HttpStatus.NOT_FOUND);
+    }
+
+    return {
+      message: "Task deleted successfully",
+    };
+  }
 }
