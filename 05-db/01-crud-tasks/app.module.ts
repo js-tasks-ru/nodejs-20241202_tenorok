aa4d9ce2f982d82b55a4b875d22f594117dd0d8b
tenorok <mail@tenorok.ru>
2025-01-25 15:29:31 +0300

    ДЗ 5.1: Реализация CRUD для задач

--- 05-db/01-crud-tasks/app.module.ts
+++ 05-db/01-crud-tasks/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { TasksModule } from "./tasks/tasks.module";
+import { Task } from "./tasks/entities/task.entity";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db.sqlite",
+      entities: [Task],
+      // Не использовать в продакшене!
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
